81e7df46d4a0ca88f60161396dfeeadfc4f4af6e
Orfeas Stefanos Thyfronitis Litos <orfeas.litos@hotmail.com>
2017-09-07 14:09:52 +0100

    Comment out "trustisrisk" tag in test

--- test/trust_is_risk.js
+++ test/trust_is_risk.js
@@ -13,9 +13,9 @@ var fixtures = require("./fixtures");
 require("should-sinon");
 
 const COIN = bcoin.consensus.COIN;
-const ripemdTag = bcoin.crypto.hash160("trustisrisk"); //
-const ripemdTag1 = "1";                                //TODO
-const tag = "1111111111111111111111111111111111";      //
+// const ripemdTag = bcoin.crypto.hash160("trustisrisk"); 
+// const ripemdTag1 = "1";                                TOFIX
+const tag = Buffer.from("1111111111111111111111111111111111");
 
 describe("TrustIsRisk", () => {
   var addr = {};
