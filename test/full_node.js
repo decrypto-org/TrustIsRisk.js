c93bcf35ca47299374987a5656b80f2172d9c07b
Orfeas Stefanos Thyfronitis Litos <orfeas.litos@hotmail.com>
2017-09-20 21:24:10 +0100

    Break down commands into smaller pieces

--- test/full_node.js
+++ test/full_node.js
@@ -184,7 +184,11 @@ describe("FullNode", () => {
       var mtx = mtxs[0];
 
       should(await mtx.verify());
-      node.sendTX(mtx.toTX());
+      var aba = mtx.toTX();
+      node.sendTX(aba);
+      await testHelpers.delay(1000000);
+      //node.sendTX(mtx.toTX());
+      console.log("aha");
 
       await testHelpers.delay(750);
       should(node.trust.getIndirectTrust(addresses.alice, addresses.bob)).equal(7 * COIN);
