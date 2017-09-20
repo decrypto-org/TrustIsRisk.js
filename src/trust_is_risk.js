df25421b17d3becdb20189a9c4ede279b6cd81c0
Orfeas Stefanos Thyfronitis Litos <orfeas.litos@hotmail.com>
2017-09-20 21:21:56 +0100

    Create short tag and public key

--- src/trust_is_risk.js
+++ src/trust_is_risk.js
@@ -17,9 +17,13 @@ class TrustIsRisk {
   node : (bcoin$FullNode | bcoin$SPVNode)
   db : TrustDB
   fakePubKeyArray : Array<number>
+  shortFakePubKeyArray : Array<number>
   fakePubKey : Buffer
+  shortFakePubKey : Buffer
   fakeKeyRing : KeyRing
+  shortFakeKeyRing : KeyRing
   tag : Buffer
+  shortTag : Buffer
 
   constructor(node : (bcoin$FullNode | bcoin$SPVNode)) {
     this.fakePubKeyArray = [0x04,                        // constant 0x04 prefix
@@ -32,10 +36,19 @@ class TrustIsRisk {
       0x6f, 0x61, 0xc3, 0x0e, 0xfc, 0x9d, 0x21, 0x91,
       0x65, 0x82, 0x80, 0x59, 0xa6, 0x01, 0x25, 0x0c,    // 32 bytes with the y coordinate
       0x8e, 0xce, 0x18, 0x00, 0x14, 0xde, 0x48, 0x1a];
-    
+
+    this.shortFakePubKeyArray = [0x02,                   // 0x02 prefix for even y values
+      0x54, 0x72, 0x75, 0x73, 0x74, 0x20, 0x69, 0x73,
+      0x20, 0x52, 0x69, 0x73, 0x6b, 0x00, 0x00, 0x00,    // only x is given in short version
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01];
+
     this.fakePubKey = Buffer.from(this.fakePubKeyArray);
+    this.shortFakePubKey = Buffer.from(this.shortFakePubKeyArray);
     this.fakeKeyRing = KeyRing.fromPublic(this.fakePubKey);
+    this.shortFakeKeyRing = KeyRing.fromPublic(this.shortFakePubKey);
     this.tag = Buffer.from(this.fakeKeyRing.getAddress("base58"));
+    this.shortTag = Buffer.from(this.shortFakeKeyRing.getAddress("base58"));
 
     this.node = node;
     this.db = new TrustDB();
