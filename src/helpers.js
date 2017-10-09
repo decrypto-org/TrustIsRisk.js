ec838444751cddcceab446f94b5600db07f6ea42
Orfeas Stefanos Thyfronitis Litos <orfeas.litos@hotmail.com>
2017-10-09 19:22:24 +0100

    Add NodeWatcher from test/helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -3,7 +3,58 @@ import type {Entity, TXHash, Key} from "./types";
 var bcoin = require("bcoin");
 var Address = bcoin.primitives.Address;
 
+class NodeWatcher {
+  txCount : number;
+  blockCount : number;
+  node : (bcoin$FullNode | bcoin$SPVNode);
+  waitForBlock : Promise<void>;
+
+  constructor(node : (bcoin$FullNode | bcoin$SPVNode)) {
+    this.txCount = 0;
+    this.blockCount = 0;
+    this.node = node;
+    this.node.on("tx", this.onTX.bind(this));
+    this.node.on("block", this.onBlock.bind(this));
+  }
+
+  onTX() : void {
+    this.txCount++;
+  }
+
+  onBlock() : void {
+    this.blockCount++;
+  }
+
+  async waitForBlock(initialCount : number) : Promise<void> {
+    if (initialCount === undefined) initialCount = this.blockCount;
+    await new Promise((resolve, reject) => {
+      var check = (() => {
+        if (this.blockCount > initialCount) resolve();
+        else setTimeout(check, 100);
+      }).bind(this);
+
+      check();
+    });
+  }
+
+  async waitForTX(initialCount : ?number) : Promise<void> {
+    var localInitialCount : number;
+    if (initialCount === undefined) localInitialCount = this.txCount;
+    else if (typeof initialCount === "number") localInitialCount = initialCount;
+    await new Promise((resolve, reject) => {
+      var check = (() => {
+        if (this.txCount > localInitialCount) resolve();
+        else setTimeout(check, 100);
+      }).bind(this);
+
+      check();
+    });
+  }
+}
+
 var helpers = {
+  NodeWatcher : NodeWatcher,
+
   pubKeyToEntity: (key : Key) : Entity => {
     return Address.fromHash(bcoin.crypto.hash160(key)).toBase58();
   },
@@ -14,5 +65,7 @@ var helpers = {
     });
   }
 };
+
+helpers.NodeWatcher = NodeWatcher;
 
 module.exports = helpers;
