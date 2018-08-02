cf9a4e02f8837f900d081c1986db58a3999ed12a
Orfeas Litos <o.thyfronitis@ed.ac.uk>
2018-08-02 23:58:03 +0100

    Connect two nodes cleanly

--- minimal_testcases/magicno.js
+++ minimal_testcases/magicno.js
@@ -9,41 +9,45 @@ async function delay(ms) {
 
 const one = new bcoin.FullNode({
   network: bcoin.Network.get().toString(),
-//  logConsole: true,
-//  logLevel: "debug",
-  httpPort: 48440,
-  port: 48441
+  //logConsole: true,
+  //logLevel: "debug",
+  httpPort: 48449
 });
 
 const two = new bcoin.FullNode({
   network: bcoin.Network.get().toString(),
-  httpPort: 48444,
   port: 48445,
   listen: true
 });
 
 (async () => {
-  await one.open()
-  await one.connect();
-
+  await one.open();
   await two.open();
+
+  await one.connect();
   await two.connect();
 
   const addr = new NetAddress({
     host: "127.0.0.1",
-    port: 48445
+    port: two.pool.options.port
   });
   const peer = one.pool.createOutbound(addr);
-  //one.pool.peers.add(peer);
-  //one.pool.setLoader(one.pool.peers.head());
+  one.pool.peers.add(peer);
 
-  await delay(3000);
+  await delay(4000);
 
-  console.log(one.pool.peers.size());
-  console.log(two.pool.peers.size());
+  for (node of [one, two])
+    if (node.pool.peers.size() !== 1)
+      throw new Error("Node " + ((node === one) ? "one" : "two") +
+        " has peer list size " + node.pool.peers.size());
+
   await two.disconnect();
-  await two.close();
-
   await one.disconnect();
+
+  await two.close();
   await one.close();
-})();
+  console.log("success!");
+})().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
