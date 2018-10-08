830dc5ab0b75f5e1638f990ed68fffc9ed310719
Orfeas Litos <o.thyfronitis@ed.ac.uk>
2018-10-08 21:52:04 +0100

    Add test for account address in bloom filter

---
+++ minimal_testcases/account_bloom.js
@@ -0,0 +1,22 @@
+const bcoin = require("bcoin");
+const WalletDB = bcoin.wallet.WalletDB;
+const NodeClient = bcoin.wallet.NodeClient;
+
+const node = new bcoin.SPVNode({});
+
+const wdb = new WalletDB({
+  client: new NodeClient(node)
+});
+
+(async () => {
+  await node.open();
+  await wdb.open();
+
+  const wallet = await wdb.create({});
+  const account = await wallet.getAccount("default");
+
+  console.log(node.pool.spvFilter.test(account.receiveAddress().getHash()));
+
+  await wdb.close();
+  await node.close();
+})();
