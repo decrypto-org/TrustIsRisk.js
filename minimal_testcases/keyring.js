38f756cb169dbf6c62d765d69e03d5ee527647c1
Orfeas Litos <o.thyfronitis@ed.ac.uk>
2018-10-11 23:53:40 +0100

    Make keyring example more robust

--- minimal_testcases/keyring.js
+++ minimal_testcases/keyring.js
@@ -2,26 +2,36 @@ const bcoin = require("bcoin");
 const WalletDB = bcoin.wallet.WalletDB;
 const KeyRing = bcoin.primitives.KeyRing;
 const secp256k1 = require("bcrypto").secp256k1;
+const assert = require("assert");
+const _ = require("lodash");
 
 (async () => {
   const wdb = new WalletDB({
     network: "testnet",
-    db: "memory"
+    db: "memory",
+    passphrase: "secret"
   });
   await wdb.open();
   const wallet = await wdb.create();
+  const account = await wallet.getAccount("default");
+  // const account2 = await wallet.ensureAccount({}, "secret");
 
-  console.log(secp256k1.privateKeyVerify(Buffer.from(wallet.master.key.xprivkey())));
-  // how to get true private key from wallet? read account.receiveAddress() and stuff...
-  console.log(Buffer.from(wallet.master.key.xprivkey()).toString("hex"));
+  const keyring = account.deriveReceive(
+    account.receiveDepth - 1, wallet.master
+  );
 
-  const privkey = secp256k1.generatePrivateKey(true);
-  console.log(privkey.toString("hex"));
+  // const privkey = secp256k1.generatePrivateKey(true);
+  const privkey = keyring.privateKey;
   const pubkey = secp256k1.publicKeyCreate(privkey, true);
-  console.log(pubkey.toString("hex"));
-  const keyring = KeyRing.fromPrivate(privkey, true);
-  console.log(keyring.getPrivateKey("hex", "regtest"));
-  console.log(keyring.getPublicKey("hex", "regtest"));
+
+  // const keyring = KeyRing.fromPrivate(privkey, true);
+
+  const privKeyFromKeyRing = keyring.getPrivateKey("hex", "regtest");
+  assert(_.isEqual(privKeyFromKeyRing, privkey.toString("hex")));
+
+  const pubKeyFromKeyRing = keyring.getPublicKey("hex", "regtest");
+  assert(_.isEqual(pubKeyFromKeyRing, pubkey.toString("hex")));
+
   const address = keyring.getAddress("base58", "regtest");
-  console.log(address);
+  console.log("success!");
 })();
